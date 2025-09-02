b683da55ece897583a756df45517e449752116de
Jon Kinney <jonkinney@gmail.com>
2025-09-01 20:53:19 -0500

    Support ARM mirrors

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -5,7 +5,13 @@ sudo pacman -S --needed --noconfirm base-devel
 
 # Configure pacman
 sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
-sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+
+# Use ARM-specific mirrorlist on ARM systems
+if [[ "$OMARCHY_ARM" == "true" ]]; then
+  sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist.arm /etc/pacman.d/mirrorlist
+else
+  sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+fi
 
 # Refresh all repos
 sudo pacman -Syu --noconfirm
