700407095ca5052e7cb9e11e34536a0913e019b1
Jon Kinney <jonkinney@gmail.com>
2026-01-21 07:33:42 -0600

    Move blueberry to AUR for ARM

--- install/config/fix-powerprofilesctl-shebang.sh
+++ install/config/fix-powerprofilesctl-shebang.sh
@@ -1,5 +1,2 @@
 # Ensure we use system python3 and not mise's python3
-# Only fix if the file exists (may not be installed on all systems)
-if [[ -f /usr/bin/powerprofilesctl ]]; then
-  sudo sed -i '/env python3/ c\#!/bin/python3' /usr/bin/powerprofilesctl
-fi
+sudo sed -i '/env python3/ c\#!/bin/python3' /usr/bin/powerprofilesctl
